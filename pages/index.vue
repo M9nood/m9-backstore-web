eda03b82cf11f4aa1056fa9d3b8c43cf5cecc802
M9nood <spw.ntw@gmail.com>
2021-10-09 14:20:18 +0700

    update rest

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>asdfsd</div>
+  <div>env {{ env }}</div>
 </template>
 
 <script lang="ts">
@@ -7,7 +7,12 @@ import Vue from 'vue'
 
 export default Vue.extend({
   asyncData({ redirect }) {
-    redirect('/login')
+    // redirect('/login')
+  },
+  data() {
+    return {
+      env: process.env.NODE_ENV
+    }
   }
 })
 </script>
